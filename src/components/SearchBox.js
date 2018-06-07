5810afec1aaf9593836bea610d1c24c88b8c746d
Daniel Liao <39768672+dtliao@users.noreply.github.com>
2018-06-08 01:34:36 +0800

    update emoji app

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,9 +6,8 @@ const SearchBox = ({ searchfield, searchChange }) => {
 		<div className='pa3'>
 			<input 
 				className='box pa3 ba w-15'
-				placeholder="&#xf002;  search emoji"
+				placeholder="&#xf002; search emoji"
 				onChange={searchChange}
-				
 			/>
 		</div>
 	);
