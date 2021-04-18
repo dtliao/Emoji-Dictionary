8a7544ae87aee1567ed74fedbf6561fa6d034707
dtliao <dtliaomr@gmail.com>
2021-04-18 10:48:30 -0500

    minor changes

--- src/components/Row.js
+++ src/components/Row.js
@@ -8,7 +8,7 @@ const Row = ({ name }) => {
 	<div className ='flex justify-center w-100'>
 		<div className ='flex pa3 f1 outline w-100'>
 			<div className="component">
-				<img className="f1 grow pointer" alt ='faces' src={`https://api.hello-avatar.com/adorables/${name}?200x200`}/>
+				<img className="f1 grow pointer" alt ='faces' src={`https://api.hello-avatar.com/adorables/${name}`} />
 				<h2 className="fr">{changedWords}</h2>
 			</div>
 		</div>
